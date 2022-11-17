af70e50e349210d65e9b94734400b46bfbc2a3ce
marcos.vale <marcos.vale@xing.com>
2022-11-17 14:06:30 +0000

    Improve search engine

--- src/Containers/Search/Search.jsx
+++ src/Containers/Search/Search.jsx
@@ -1,20 +1,23 @@
 import { useEffect, useState } from 'react';
 import './Search.css';
-import SearchBar from '../../Components/Searchbar/Searchbar';
 import { searchFilms } from '../../Services/apicalls';
 import Moviedetail from '../../Components/Moviedetail/Moviedetail';
 
 const Search = () => {
     const [movies, setMovies] = useState([])
-    const query = new URLSearchParams('./search').get('s')
-    const [searchQuery, setSearchQuery] = useState(query || '')
+    const [searchQuery, setSearchQuery] = useState('')
     const [choosen, setChoosen] = useState("");
 
     useEffect(() => {
-        searchFilms(searchQuery)
-            .then(res => setMovies(res.data.results))
-            .catch(error => console.log(error))
+        if (searchQuery !== "") {
+            const bring = setTimeout(() => {
+                searchFilms(searchQuery)
+                    .then(res => setMovies(res.data.results))
+                    .catch(error => console.log(error))
+            }, 500);
 
+            return () => clearTimeout(bring)
+        }
     }, [searchQuery])
 
     const selectFilm = (film) => {
@@ -22,44 +25,31 @@ const Search = () => {
         setChoosen(film)
     }
 
-    if (movies.length === 0) {
-        return (
-            <div className='searchPage'>
-                <SearchBar
-                    searchQuery={searchQuery}
-                    setSearchQuery={setSearchQuery}
-                />
-            </div>
-        )
-    } else {
-        return (
-            <div className='searchPage'>
-                <SearchBar
-                    searchQuery={searchQuery}
-                    setSearchQuery={setSearchQuery}
-                />
-                <div className='moviesShowcase'>
-                    <div className='leftSide'>
-                        {
-                            movies.map(film => {
-                                return <div className='filmDesign' key={film.id}>
-                                    <div onClick={() => selectFilm(film)}><img className="filmPic" src={'https://www.themoviedb.org/t/p/w440_and_h660_face' + film.backdrop_path} alt={film.original_title} /></div>
-                                    <div>{film.title}</div>
-                                </div>
-                            })
-                        }
-                    </div>
-                    <div className='rightSide'>
-                        {
-                            choosen?.id !== undefined &&
-                            //I am summoning the moviedetail component, passing the whole selected movie to it using props
-                            <Moviedetail choosen={choosen} />
-                        }
-                    </div>
+    return (
+        <div className='searchPage'>
+            <input name='search' placeholder='Search movie' className='' value={searchQuery}
+                    onInput={e => setSearchQuery(e.target.value)}/>
+            <div className='moviesShowcase'>
+                <div className='leftSide'>
+                    {
+                        movies.map(film => {
+                            return <div className='filmDesign' key={film.id}>
+                                <div onClick={() => selectFilm(film)}><img className="filmPic" src={'https://www.themoviedb.org/t/p/w440_and_h660_face' + film.backdrop_path} alt={film.original_title} /></div>
+                                <div>{film.title}</div>
+                            </div>
+                        })
+                    }
+                </div>
+                <div className='rightSide'>
+                    {
+                        choosen?.id !== undefined &&
+                        //I am summoning the moviedetail component, passing the whole selected movie to it using props
+                        <Moviedetail choosen={choosen} />
+                    }
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Search;
